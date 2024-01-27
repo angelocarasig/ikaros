da8de11dfef3cbcd9594cf90949decb107c72acb
Angelo Carasig <s3906344@student.rmit.edu.au>
2024-01-28 09:26:15 +1100

    Tweak auth backend controller

--- astraea/src/app/controllers/auth/auth.controller.ts
+++ astraea/src/app/controllers/auth/auth.controller.ts
@@ -1,9 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SupabaseService } from '../../services/supabase/supabase.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly supabaseService: SupabaseService) {}
+  @Get()
+  healthCheck() {
+    return { message: 'Auth route is working!'};
+  }
 
   @Post('login')
   async loginWithPassword(@Body() body: { email: string; password: string }) {
